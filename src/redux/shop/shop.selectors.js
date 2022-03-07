b7ed39b6acc02f990afbaa45f78bb9af84202335
gaurang-tcs <98448274+gaurang-tcs@users.noreply.github.com>
2022-03-07 17:28:54 +0530

    Implement of redux thunk and Debugging our code which is isuue from is Loading is solved(18th module completed)

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -18,3 +18,13 @@ export const selectCollection = collectionUrlParam =>
        [selectCollections],
        collections => (collections ? collections[collectionUrlParam] : null)
    );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+)
