99af2f7eeb438775c9e7c21149a486111ef9b3e9
gaurang-tcs <98448274+gaurang-tcs@users.noreply.github.com>
2022-03-08 17:42:49 +0530

    done until redux thunk in to saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -24,5 +24,5 @@ export const fetchCollectionsStartAsync = () => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             dispatch(fetchCollectionsSuccess(collectionsMap));
         }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
-    }
+    };
 }
